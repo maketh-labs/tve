21d33737a9f86b35540fe09d61a2a26437a5b5b8
Ion Jo <monsieur.mansa.musa@gmail.com>
2025-03-25 03:38:16 +0700

    chore: rename Counter -> TriangleVotingEscrow

---
+++ test/TriangleVotingEscrow.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Test, console} from "forge-std/Test.sol";
+import {TriangleVotingEscrow} from "../src/TriangleVotingEscrow.sol";
+
+contract TriangleVotingEscrowTest is Test {
+    TriangleVotingEscrow public tve;
+
+    function setUp() public {
+        tve = new TriangleVotingEscrow();
+    }
+}
