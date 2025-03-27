fa0db78eddb7e5e344b70a2726afbd1a3e8b6130
Ion Jo <monsieur.mansa.musa@gmail.com>
2025-03-28 05:36:24 +0700

    fix: calculate area logic

--- src/TriangleVotingEscrow.sol
+++ src/TriangleVotingEscrow.sol
@@ -21,20 +21,21 @@ contract TriangleVotingEscrow {
     }
 
     // epochs is the number of full epochs to stake for
-    function stake(address user, uint256 amount, uint256 epochs) public {
-        uint256 unlocked = calculateUnlocked(user, block.timestamp);
+    function vest(address user, uint256 amount, uint256 epochs) public {
+        uint256 currentEpoch = block.timestamp / epochLength;
+
+        // Check if we need to transfer additional tokens
+        uint256 unlocked = unlockedAt(user, block.timestamp);
         if (amount > unlocked) {
             IERC20(token).transferFrom(msg.sender, address(this), amount - unlocked);
             balanceOf[user] += amount - unlocked;
         }
 
-        uint256 currentEpoch = block.timestamp / epochLength;
-
-        // Get height of triangle at beginning of current epoch. Should be larger than `amount`, which is the height at the current block timestamp.
-        // height / (epochs + 1) * epochLength = amount / lastEpochEnd - block.timestamp
+        // Calculate initial height of triangle
         uint256 lastEpochEnd = (currentEpoch + epochs + 1) * epochLength;
         uint256 height = amount * (epochs + 1) * epochLength / (lastEpochEnd - block.timestamp);
 
+        // Update escrow records for each epoch
         for (uint256 i = 0; i < epochs + 1; i++) {
             totalEscrowedAt[currentEpoch + i] += height;
             escrowedAt[user][currentEpoch + i] += height;
@@ -53,33 +54,67 @@ contract TriangleVotingEscrow {
     }
 
     function calculateArea(mapping(uint256 epoch => uint256) storage e, uint256 time) internal view returns (uint256) {
-        uint256 currentEpoch = time / epochLength;
-        uint256 height = e[currentEpoch];
-        uint256 area = 0;
-        while (height > 0) {
-            area += height * epochLength;
-            currentEpoch += 1;
-            height = e[currentEpoch];
-            area += height * epochLength;
+        uint256 epoch = time / epochLength;
+        uint256 currentTimeInEpoch = time % epochLength;
+
+        uint256 left = e[epoch];
+        uint256 right = e[epoch + 1];
+
+        // Calculate area of current partial epoch
+        uint256 currentHeight = interpolateHeight(left, right, epochLength, currentTimeInEpoch);
+        uint256 area = (currentHeight + right) * (epochLength - currentTimeInEpoch) / 2;
+
+        // Add areas of future epochs
+        while (right > 0) {
+            epoch++;
+            left = right;
+            right = e[epoch + 1];
+            area += (left + right) * epochLength / 2;
         }
+
         return area;
     }
 
-    function withdraw(uint256 amount) public {
-        uint256 unlocked = calculateUnlocked(msg.sender, block.timestamp);
+    function unlockedAt(address user, uint256 time) public view returns (uint256) {
+        uint256 currentEpoch = time / epochLength;
+        uint256 balance = balanceOf[user];
+        uint256 height = interpolateHeight(
+            escrowedAt[user][currentEpoch], escrowedAt[user][currentEpoch + 1], epochLength, time % epochLength
+        );
+        return balance - height;
+    }
+
+    function claim(uint256 amount) public {
+        uint256 unlocked = unlockedAt(msg.sender, block.timestamp);
         require(amount <= unlocked, "Not enough unlocked");
         IERC20(token).transfer(msg.sender, amount);
         balanceOf[msg.sender] -= amount;
     }
 
-    // Only works if time is after the start of the first epoch
-    function calculateUnlocked(address user, uint256 time) public view returns (uint256) {
-        uint256 currentEpoch = time / epochLength;
-        uint256 left = escrowedAt[user][currentEpoch];
-        uint256 right = escrowedAt[user][currentEpoch + 1];
+    function claimAll() public {
+        uint256 unlocked = unlockedAt(msg.sender, block.timestamp);
+        require(unlocked > 0, "None unlocked");
+        IERC20(token).transfer(msg.sender, unlocked);
+        balanceOf[msg.sender] -= unlocked;
+    }
 
-        // find the height within the trapizoid for corresponding time
-        uint256 height = (left * ((currentEpoch + 1) * epochLength - time) + right * (currentEpoch * epochLength - time)) / epochLength;
-        return balanceOf[user] - height;
+    /**
+     * @notice Calculates the height of a line within a trapezoid using linear interpolation
+     * @param leftHeight Height of the left side of the trapezoid
+     * @param rightHeight Height of the right side of the trapezoid
+     * @param totalWidth Total width of the trapezoid (time period)
+     * @param currentPosition Current position within the trapezoid (current time)
+     * @return Height of the interpolated line at the current position
+     */
+    function interpolateHeight(uint256 leftHeight, uint256 rightHeight, uint256 totalWidth, uint256 currentPosition)
+        public
+        pure
+        returns (uint256)
+    {
+        // Linear interpolation formula:
+        // height = leftHeight + (rightHeight - leftHeight) * (currentPosition / totalWidth)
+        // This can be rearranged to avoid floating point:
+        // height = (leftHeight * (totalWidth - currentPosition) + rightHeight * currentPosition) / totalWidth
+        return (leftHeight * (totalWidth - currentPosition) + rightHeight * currentPosition) / totalWidth;
     }
 }
--- test/TriangleVotingEscrow.sol
+++ test/TriangleVotingEscrow.sol
@@ -3,11 +3,47 @@ pragma solidity ^0.8.28;
 
 import {Test, console} from "forge-std/Test.sol";
 import {TriangleVotingEscrow} from "../src/TriangleVotingEscrow.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract TriangleVotingEscrowTest is Test {
+    ERC20Mock public token;
     TriangleVotingEscrow public tve;
+    address public user;
 
     function setUp() public {
-        tve = new TriangleVotingEscrow(address(0), 30 days);
+        token = new ERC20Mock();
+        tve = new TriangleVotingEscrow(address(token), 30 days);
+        user = makeAddr("user");
+
+        token.mint(user, 1000 ether);
+        vm.prank(user);
+        token.approve(address(tve), 1000 ether);
+
+        vm.warp(45 days);
+    }
+
+    function test_interpolateHeight() public {
+        assertEq(tve.interpolateHeight(1000 ether, 0, 30 days, 0), 1000 ether, "start at 1000 ether");
+        assertEq(tve.interpolateHeight(1000 ether, 0, 30 days, 6 days), 800 ether, "200 ether at 6 days");
+        assertEq(tve.interpolateHeight(1000 ether, 0, 30 days, 12 days), 600 ether, "400 ether at 12 days");
+        assertEq(tve.interpolateHeight(1000 ether, 0, 30 days, 15 days), 500 ether, "500 ether at 15 days");
+        assertEq(tve.interpolateHeight(1000 ether, 0, 30 days, 18 days), 400 ether, "400 ether at 18 days");
+        assertEq(tve.interpolateHeight(1000 ether, 0, 30 days, 24 days), 200 ether, "200 ether at 24 days");
+        assertEq(tve.interpolateHeight(1000 ether, 0, 30 days, 30 days), 0 ether, "0 ether at 30 days");
+    }
+
+    function test_vest() public {
+        vm.prank(user);
+        tve.vest(user, 1000 ether, 0);
+
+        // Sanity check
+        assertEq(token.balanceOf(user), 0, "user should have 0 balance");
+        assertEq(token.balanceOf(address(tve)), 1000 ether, "tve should have 1000 ether");
+
+        // Check voting power
+        assertEq(tve.balanceOf(user), 1000 ether, "user should have 1000 ether");
+        assertEq(tve.totalEscrowedAt(1), 1000 ether * 2, "totalEscrowedAt should be 1000 ether");
+        assertEq(tve.escrowedAt(user, 1), 1000 ether * 2, "escrowedAt should be 1000 ether");
+        assertEq(tve.votingPowerOf(user, block.timestamp), 1000 ether * 15 days / 2, "votingPower");
     }
 }
